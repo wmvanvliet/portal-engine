ef91d7ecba98e3397a10d6b1323cbb7db85ed365
Marijn van Vliet <w.m.vanvliet@gmail.com>
2019-07-15 09:56:17 +0300

    Add windows build script

---
+++ make.bat
@@ -0,0 +1,5 @@
+set CC="C:\Program Files\mingw-w64\x86_64-8.1.0-posix-seh-rt_v6-rev0\mingw64\bin\gcc"
+set INCLUDE_DIR=sdl2\include
+set LIB_DIR=sdl2\lib\x64
+set LIBS=-lSDL2main -lSDL2
+%CC% -I%INCLUDE_DIR% -L%LIB_DIR% -o engine.exe engine.c -Wall %LIBS%
